9ce4c58421aa7dbbb73e3a6a172c964635a1f54b
Tejas Nikhar <tejastn10@gmail.com>
2025-12-02 07:22:00 +0530

    feat: add main redis functions

--- src/pkg/cache/redis.go
+++ src/pkg/cache/redis.go
@@ -2,6 +2,8 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -43,4 +45,66 @@ func (r *Redis) Close() error {
 
 func (r *Redis) Health(ctx context.Context) error {
 	return r.Client.Ping(ctx).Err()
+}
+
+// <-------------------- Main Functions -------------------->
+
+func (r *Redis) GetClient() *redis.Client {
+	return r.Client
+}
+
+func (r *Redis) Set(ctx context.Context, key string, value any, ttl time.Duration) error {
+	if ttl == 0 {
+		ttl = r.DefaultTTL
+	}
+
+	data, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("failed to marshal value: %w", err)
+	}
+
+	return r.Client.Set(ctx, key, data, ttl).Err()
+}
+
+func (r *Redis) Get(ctx context.Context, key string, dest any) error {
+	data, err := r.Client.Get(ctx, key).Result()
+	if err != nil {
+		return fmt.Errorf("failed to get value: %w", err)
+	}
+
+	if err := json.Unmarshal([]byte(data), dest); err != nil {
+		return fmt.Errorf("failed to unmarshal value :%w", err)
+	}
+
+	return nil
+}
+
+func (r *Redis) Delete(ctx context.Context, key string) error {
+	return r.Client.Del(ctx, key).Err()
+}
+
+func (r *Redis) Exists(ctx context.Context, key string) (bool, error) {
+	count, err := r.Client.Exists(ctx, key).Result()
+	return count > 0, err
+}
+
+func (r *Redis) Increment(ctx context.Context, key string) (int64, error) {
+	return r.Client.Incr(ctx, key).Result()
+}
+
+func (r *Redis) IncrementBy(ctx context.Context, key string, value int64) (int64, error) {
+	return r.Client.IncrBy(ctx, key, value).Result()
+}
+
+func (r *Redis) SetNX(ctx context.Context, key string, value any, ttl time.Duration) (bool, error) {
+	if ttl == 0 {
+		ttl = r.DefaultTTL
+	}
+
+	data, err := json.Marshal(value)
+	if err != nil {
+		return false, fmt.Errorf("failed to marshal value: %w", err)
+	}
+
+	return r.Client.SetNX(ctx, key, data, ttl).Result()
 }
